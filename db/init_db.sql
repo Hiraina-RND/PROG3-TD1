e8a7280a8f13addf20db1ed1b76165a4ed2ff9eb
Hiraina <hei.hiraina@gmail.com>
2025-12-05 22:59:43 +0300

    fix(init_db.sql): add connection to database product_management_db before granting CREATE to product_manager_user

--- db/init_db.sql
+++ db/init_db.sql
@@ -2,4 +2,6 @@ CREATE DATABASE "product_management_db";
 
 CREATE USER "product_manager_user" WITH PASSWORD '123456';
 
+\c "product_management_db"
+
 GRANT CREATE ON SCHEMA public TO "product_manager_user";
