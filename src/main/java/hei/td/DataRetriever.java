4c7249daec3afe33ad0b9c10fc73d0b8c4593aaf
Hiraina <hei.hiraina@gmail.com>
2025-12-09 20:07:32 +0300

    add getProductsByCriteria method with pagination for DataRetriever class

--- src/main/java/hei/td/DataRetriever.java
+++ src/main/java/hei/td/DataRetriever.java
@@ -196,4 +196,97 @@ public class DataRetriever {
         }
         return filteredProducts;
     }
+
+    List<Product> getProductsByCriteria(String productName, String categoryName, Instant creationMin, Instant creationMax, int page, int size) throws SQLException {
+        List<Product> filteredProducts = new ArrayList<>();
+
+        DBConnection dbConnection = new DBConnection(
+                "jdbc:postgresql://localhost:5432/product_management_db",
+                "product_manager_user",
+                "123456"
+        );
+
+        StringBuilder sql = new StringBuilder();
+
+        if (productName != null) {
+            sql = new StringBuilder("""
+                        SELECT p.id, p.name, p.creation_datetime, c.name AS category_name
+                        FROM product p
+                        INNER JOIN product_category c
+                        ON c.product_id = p.id
+                        WHERE 1=1
+        """);
+
+            sql.append(" AND p.name ILIKE ? ");
+
+            if (categoryName != null) {
+                sql.append(" AND c.name ILIKE ? ");
+            }
+        } else {
+            sql = new StringBuilder("""
+                        SELECT p.id, p.name, p.creation_datetime, c.name
+                        FROM product p
+                        INNER JOIN product_category c
+                        ON c.product_id = p.id
+                        WHERE 1=1
+        """);
+
+            if (categoryName != null) {
+                sql.append(" AND c.name ILIKE ? ");
+            }
+        }
+
+        if (creationMin != null) {
+            sql.append(" AND p.creation_datetime >= ? ");
+        }
+
+        if (creationMax != null) {
+            sql.append(" AND p.creation_datetime <= ? ");
+        }
+
+        sql.append(" ORDER BY p.id ");
+        sql.append(" LIMIT ? OFFSET ? ");
+
+
+        try (
+                Connection connection = dbConnection.getDBConnection();
+                PreparedStatement ps = connection.prepareStatement(sql.toString());
+        ) {
+
+            int paramIndex = 1;
+
+            if (productName != null && categoryName == null) {
+                ps.setString(paramIndex++, "%" + productName + "%");
+            }
+
+            if (productName != null && categoryName != null) {
+                ps.setString(paramIndex++, "%" + productName + "%");
+                ps.setString(paramIndex++, "%" + categoryName + "%");
+            }
+
+            if (productName == null && categoryName != null) {
+                ps.setString(paramIndex++, "%" + categoryName + "%");
+            }
+
+            if (creationMin != null) {
+                ps.setTimestamp(paramIndex++, Timestamp.from(creationMin));
+            }
+
+            if (creationMax != null) {
+                ps.setTimestamp(paramIndex++, Timestamp.from(creationMax));
+            }
+
+            ps.setInt(paramIndex++, size);
+            ps.setInt(paramIndex++, page * size);
+
+            try (ResultSet resultSet = ps.executeQuery()) {
+                while (resultSet.next()) {
+                    filteredProducts.addAll(mapToProducts(resultSet));
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error executing query", e);
+        }
+        return filteredProducts;
+    }
 }
